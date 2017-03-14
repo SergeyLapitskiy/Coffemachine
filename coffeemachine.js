d57365b13c6e1f661265168cc695d866c34d7ab0
PC <lapitsky94@mail.ru>
2017-03-14 13:54:25 +0300

    commit

--- coffeemachine.js
+++ coffeemachine.js
@@ -1,22 +1,18 @@
 products = [//создает массив всех продуктов
 	{
 		name: "Cappuchino",
-		id: "cappuchino",
 		price: 100
 	},
 	{
 		name: "Americano",
-		id: "americano",
 		price: 110
 	},
 	{
 		name: "Espresso",
-		id: "espresso",
 		price: 120
 	},
 	{
 		name: "Latte",
-		id: "latte",
 		price: 130
 	},
 	{
@@ -26,75 +22,84 @@ products = [//создает массив всех продуктов
 	},
 	{
 		name: "Macchiato",
-		id: "macchiato",
 		price: 150
 	},
 	{
 		name: "Mokko",
-		id: "moka",
 		price: 160
 	},
 	{
 		name: "Hotchocolate",
-		id: "hot-chocolate",
 		price: 170
 	}
 ];
 
 for(var i = 0; i < products.length; i++){
     var product = products[i];// переменная product - это один элемент массива products
-    drinkTable.innerHTML += ` 
-	<tr><td>${product['name']}</td> <td>${product['price']}р</td> <td><input type="checkbox" class="mCheckbox"
-		
-	></td></tr><br>
-    `
+    drinkTable.innerHTML += `
+	<tr><td>${product['name']}</td> <td>${product['price']}р</td>
+		<td><input type="checkbox" class="mCheckbox" value=${product['price']}></td></tr><br>`
 
 };
 
 var mCofemachine = new Cofemachine(0, 0);
-/*var mCheckbox = document.getElementsByClassName('mCheckbox');
-console.log(mCheckbox);
-window.onclick = function() {
-    for(var i=0; i<mCheckbox.length; i++){
-        if (mCheckbox[i].checked) {
-      		console.log(products[i].price);
-    	}
-	}
-}*/
+var mCheckbox = document.getElementsByClassName('mCheckbox');
+for(var i=0; i<mCheckbox.length;i++){
+	mCheckbox[i].onchange = function(event){
+			if(event.target.checked){
+				mCofemachine.addCofe(parseInt(event.target.value));
+			}else{
+				mCofemachine.addCofe(parseInt(-event.target.value));
+			}
+		}
+}
 
 var money = document.getElementById('money');
 money.addEventListener("change", function(e){
-	mCofemachine.addCashe(money.value);
+	mCofemachine.setCashe(money.value);
 });
 var mButton = document.getElementById('mButton');
 mButton.addEventListener("click", function(e){
-	itog.innerHTML = `Итого = ${mCofemachine.cPrice()}`
-	
+	if(mCofemachine.CofePrice()>mCofemachine.getCashe()){
+		itog.innerHTML = `Недостаточно средств`;
+	}else{
+		itog.innerHTML = `Итого = ${mCofemachine.CofePrice()}`
+		sdacha.innerHTML = `Сдача = ${mCofemachine.getBackMoney()}`
+	}
+
 });
 
 document.getElementById('sugar').onclick = function() {
-	console.log(mCofemachine.addSugar(this.checked));
-
+	mCofemachine.addSugar(this.checked);
 }
 
 
 function Cofemachine(cofePrice, clientCashe){
-	this.clientCasshe = clientCashe;
+	this.clientCashe = clientCashe;
 	this.cofePrice = cofePrice;
 
 	this.addSugar = function(IsChecked){
 		if(IsChecked){
 			cofePrice += 10;
-			return cofePrice; 
-		} 
-		return cofePrice -=10;
+		}else{
+			cofePrice -=10;
+		}
 	}
-	this.cPrice = function(){
-		return cofePrice; 
+	this.addCofe = function(cofeP){
+		console.log(cofeP);
+		cofePrice += cofeP;
 	}
-	this.addCashe = function(cashe){
-		console.log(cashe);
-		clientCasshe = cashe;
+	this.CofePrice = function(){
+		return cofePrice;
+	}
+	this.setCashe = function(cashe){
+		clientCashe = cashe;
+	}
+	this.getCashe = function(){
+		return clientCashe
+	}
+	this.getBackMoney = function(){
+		return (clientCashe-cofePrice);
 	}
 
 }
